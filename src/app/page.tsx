069f1ae84ff026190e87e8a01acf048699568397
Epaphra-B <epaphra12@gmail.com>
2025-07-09 15:55:57 +0530

    replaced quote

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
             <Link
               key={item.href}
               href={item.href}
-              className="text-base duration-500 text-zinc-500 hover:text-zinc-300"
+              className="text-base duration-500 text-zinc-500 hover:text-zinc-300 cursor-default"
             >
               {item.name}
             </Link>
@@ -34,15 +34,8 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 ">
-          I&apos;m building{" "}
-          <Link
-            target="_blank"
-            href="https://unkey.dev"
-            className="underline duration-500 hover:text-zinc-300"
-          >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+        <h2 className="text-sm text-zinc-500 cursor-default ">
+          Designing with purpose. Developing with precision.
         </h2>
       </div>
     </div>
