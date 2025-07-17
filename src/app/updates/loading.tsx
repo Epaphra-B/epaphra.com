4a37c3aaeb31bca1b946a70c8e6e3dcfb4723ad5
Epaphra-B <epaphra12@gmail.com>
2025-07-17 19:40:32 +0530

    new commit

--- src/app/updates/loading.tsx
+++ src/app/updates/loading.tsx
@@ -1,49 +1,9 @@
-
-import { motion } from "framer-motion"
+import LoadingThreeDotsJumping from "@/app/components/bouncingDots";
 
 export default function Loading() {
-    return (
-         <div className="container">
-            <motion.div
-                className="spinner"
-                animate={{ rotate: 360 }}
-                transition={{
-                    duration: 1.5,
-                    repeat: Infinity,
-                    ease: "linear",
-                }}
-            />
-            <StyleSheet />
-        </div>
-    )
-}
-
-
-
-/**
- * ==============   Styles   ================
- */
-function StyleSheet() {
-    return (
-        <style>
-            {`
-            .container {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                padding: 40px;
-                border-radius: 8px;
-            }
-
-            .spinner {
-                width: 50px;
-                height: 50px;
-                border-radius: 50%;
-                border: 4px solid var(--divider);
-                border-top-color: #ff0088;
-                will-change: transform;
-            }
-            `}
-        </style>
-    )
+  return (
+    <div className="h-screen w-full flex items-center justify-center bg-black">
+      <LoadingThreeDotsJumping />
+    </div>
+  );
 }
