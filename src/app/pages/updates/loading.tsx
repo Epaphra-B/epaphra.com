be8a260535ec6aafc11e9ed51b11e1240b3baad3
Epaphra-B <epaphra12@gmail.com>
2025-07-25 13:05:36 +0530

    rm loading in updates

--- src/app/pages/updates/loading.tsx
+++
@@ -1,9 +0,0 @@
-import LoadingThreeDotsJumping from "@/app/components/bouncingDots";
-
-export default function Loading() {
-  return (
-    <div className="h-screen w-full flex items-center justify-center bg-black">
-      <LoadingThreeDotsJumping />
-    </div>
-  );
-}
