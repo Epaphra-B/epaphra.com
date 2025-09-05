168576f2a57d105a209d8d46c178ef98056352bc
Epaphra-B <epaphra12@gmail.com>
2025-09-05 12:12:40 +0530

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import Link from "next/link";
+// import Link from "next/link";
 import React from "react";
 import Particles from "./components/particles";
 
-const navigation = [
-  { name: "Projects", href: "pages/updates" },
-  { name: "Contact", href: "pages/contact" },
-];
+// const navigation = [
+//   { name: "Projects", href: "pages/updates" },
+//   { name: "Contact", href: "pages/contact" },
+// ];
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center w-screen h-screen overflow-hidden bg-gradient-to-tl from-black via-zinc-600/20 to-black">
-      <nav className="my-16 animate-fade-in">
+      {/* <nav className="my-16 animate-fade-in">
         <ul className="flex items-center justify-center gap-4">
           {navigation.map((item) => (
             <Link
@@ -22,21 +22,21 @@ export default function Home() {
             </Link>
           ))}
         </ul>
-      </nav>
+      </nav> */}
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-left bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <Particles
         className="absolute inset-0 -z-10 animate-fade-in w-100% h-100%"
         quantity={200}
       />
-      <h1 className=" z-10 text-4xl text-transparent duration-1000 bg-white cursor-default text-edge-outline animate-title font-calsans sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
-        epaphra
+      <h1 className=" z-10 text-wrap text-4xl text-transparent duration-1000 text-clip bg-[linear-gradient(173deg,_rgb(255,255,255)_28.4594%,_rgba(3,3,3,0)_108%)] cursor-default text-edge-outline animate-title font-calsans sm:text-6xl md:text-9xl whitespace-nowrap bg-clip-text ">
+        DVSR Prasad
       </h1>
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        <h2 className="text-sm text-zinc-500 cursor-default ">
+        {/* <h2 className="text-sm text-zinc-500 cursor-default ">
           Designing with purpose. Developing with precision.
-        </h2>
+        </h2> */}
       </div>
     </div>
   );
---
+++ src/app/pages/weather/page.tsx
@@ -0,0 +1,18 @@
+
+export default function WeatherPage() {
+    return(      
+        <div>
+            <div className="fixed h-screen w-15 rounded-3xl border-1  border-white">
+                <nav className="flex flex-col items-center p-2 h-full">
+                    <ul>
+                        <li>li</li>
+                        <li>li</li>
+                        <li>li</li>
+                        <li>li</li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    
+    );
+}
