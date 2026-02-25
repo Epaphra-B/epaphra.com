594f38dce78b466ba0193c3ae4a2aee62fb81148
Dee <epaphra12@gmail.com>
2026-02-25 23:49:29 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,9 +34,9 @@ export default function Home() {
 
       <div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
       <div className="my-16 text-center animate-fade-in">
-        {/* <h2 className="text-sm text-zinc-500 cursor-default ">
+        <h2 className="text-sm text-zinc-500 cursor-default ">
           Designing with purpose. Developing with precision.
-        </h2> */}
+        </h2>
       </div>
     </div>
   );
